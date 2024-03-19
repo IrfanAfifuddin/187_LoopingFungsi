5f2b7195e0f9f6d15716e66600ec3dc8110e2bff
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-03-19 10:37:43 +0700

    membuat for dan cout data array ke-

--- 187_LoopingFungsi.cpp
+++ 187_LoopingFungsi.cpp
@@ -16,6 +16,8 @@ int main() {
         cout << "Masukan Nilai index ke-" << i << " :";
         cin >> arr[i];
     }
-   
+    for (i = 0; i < 5; i++) {
+        cout << "Data Array ke-" << i << " :" << arr[i] << endl;
+    }
     
 }
